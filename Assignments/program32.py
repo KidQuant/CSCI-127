a47158c4b037b28c7000717196b49eb61966faa4
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-03-18 22:08:10 -0400

    updates for new code

--- Assignments/program32.py
+++ Assignments/program32.py
@@ -4,15 +4,15 @@
 # This program zooms in on an section of the image
 
 import matplotlib.pyplot as plt
-import numpy as np
 
-inF = input("Enter file name: ")
+inputFile = input("Enter image file name: ")
+outputFile = input("Enter output file: ")
 
-img = plt.imread(inF)
+img = plt.imread(inputFile)
 
 height = img.shape[0]
 width = img.shape[1]
 
-img2 = img[height // 4 :, width // 4]
+img2 = img[height // 2 :, : width // 2]
 
-plt.imsave(img2)
+plt.imsave(outputFile, img2)
