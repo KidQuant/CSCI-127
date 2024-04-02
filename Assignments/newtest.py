65e70ed1c5ac25ec62253c788cfcb71a257154a4
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-04-01 21:25:06 -0400

    updates to test file

--- Assignments/newtest.py
+++ Assignments/newtest.py
@@ -1,8 +1,59 @@
-# Sample program for looping
+# Intro Programming Lab: A propgram with a herd of turtles
 
-magicians = ["alice", "david", "carolina"]
-for magician in magicians:
-    print(f"{magician.title()}, that was a great trick!")
-    print(f"I can't wait to see you next trick, {magician.title()}.\n")
+import turtle
 
-print("Thank you, everyone. That was a great magic show!")
+
+def welcomeMessage():
+    print()
+    print("Welcome to our herd of turtles demonstration!")
+    print()
+    return
+
+
+def getInput():
+    n = eval(input("Please enter number of turtles: "))
+    return n
+
+
+def setUpScreen():
+    w = turtle.Screen()
+    w.bgcolor()
+    return w
+
+
+def setUpTurtles(n):
+    tList = []
+    for i in range(n):
+        t = turtle.Turtle()
+        t.shape("turtle")
+        tList.append(t)
+
+    for i in range(n):
+        tList[i].color(0, 0, i / (2 * n) + 0.5)
+        tList[i].right(i * 360 / n)
+
+    return tList
+
+
+def moveForward(tList):
+    for t in tList:
+        t.forward(30)
+
+
+def stamp(tList):
+    for t in tList:
+        t.stamp()
+
+
+def main():
+    welcomeMessage()
+    numTurtles = getInput()
+    w = setUpScreen()
+    turtleList = setUpTurtles(numTurtles)
+    for i in range(10):
+        moveForward(turtleList)
+        stamp(turtleList)
+
+
+if __name__ == "__main__":
+    main()
