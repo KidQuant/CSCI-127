52151d794c77cf662c71363ae1a9983e25605fb4
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-04-08 17:10:26 -0400

    updates

--- Assignments/program38.py
+++ Assignments/program38.py
@@ -2,3 +2,12 @@
 # Email: andre.sealy72@myhunter.cuny.edu
 # Date: April 8th, 2024
 # List the top 3 contributing factors for collision
+
+import pandas as pd
+
+csvFile = input("Enter CSV file name: ")
+
+df = pd.read_csv(csvFile)
+
+print("Top three contributing factors for collisions:")
+print(df["CONTRIBUTING FACTOR VEHICLE 1"].value_counts()[:3])
