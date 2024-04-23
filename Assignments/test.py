dcb1da7a9d7447829fedb0c715faab9c8ab7d341
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-04-23 13:09:53 -0400

    updates to commit

--- Assignments/test.py
+++ Assignments/test.py
@@ -1,16 +1,5 @@
-import folium
-import pandas as pd
-
-cuny = pd.read_csv("CUNYcampuses.csv")
-print(cuny["Campus"])
-
-mapCUNY = folium.Map(location=[40.768731, -73.964915])
-
-for index, row in cuny.iterrows():
-    lat = row["Latitude"]
-    lon = row["Longitude"]
-    name = row["Campus"]
-    newMarker = folium.Marker([lat, lon], popup=name)
-    newMarker.add_to(mapCUNY)
-
-mapCUNY.save(outfile="cunyLocations.html")
+age = int(input("Please enter age: "))
+while age < 0:
+    print("Entered a negative number.")
+    age = int(input("Please enter age: "))
+print("You entered your age as:", age)
